e806aea016ab503703f67af450acf5742dae1f07
Yang <sarahyang@rocketmortgage.com>
2022-11-22 10:49:52 -0800

    added most of the features, adding conditionals for css

--- routing-app/src/app/app.component.ts
+++ routing-app/src/app/app.component.ts
@@ -2,7 +2,7 @@ import { HttpClient } from '@angular/common/http';
 import { Component } from '@angular/core';
 import { Ticket } from './ticket';
 import { TicketService } from './ticket.service';
-
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-root',
@@ -11,7 +11,7 @@ import { TicketService } from './ticket.service';
 })
 export class AppComponent {
   title = 'HelpDesk';
-
+  constructor(public router: Router) { }
   // constructor(public http: HttpClient, public ticketSrv: TicketService) { }
 
   // finishedWithCall(obj: any) {
